802892048d9f84f61c43c3e3d02d021dac436dcf
Joaquin Herrera Aranguiz <42761019+Asdii@users.noreply.github.com>
2022-03-23 04:55:32 -0300

    Corrección
    Linea 6-7: Olvidaste inicializar el generador de números al azar.
    Linea 10: La condición debe llevar la variable ("rata") generada, no un número al azar nuevo.

--- ruleta-rusa.c
+++ ruleta-rusa.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 int main(int argc, char const *argv[]) {
   int rata;
-  rata = rand()%6;
-  if ( rand() == 1){
+  
+  time_t t;
+  srand((unsigned) time(&t));
+  
+  rata = rand() % 6;
+  if ( rata == 1){
     printf("I am a fucking rat\n");
   }
   else{
